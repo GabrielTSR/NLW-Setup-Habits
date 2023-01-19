6133a48766a136e8c8e7727a914ceee5f4b8d4b7
GabrielTSR <gabrielalvesrocha22@gmail.com>
2023-01-18 21:11:16 -0300

    feat: making HabitDay, Header, SummaryTable (front-end web components)

--- web/src/components/Habit.tsx
+++
@@ -1,7 +0,0 @@
-interface HabitProps {
-    completed: number
-}
-
-export function Habit({ completed }: HabitProps) {
-    return <p className="bg-zinc-900 w-10 h-10 text-tomato">{completed}</p>
-}
