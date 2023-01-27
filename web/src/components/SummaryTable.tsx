bf98c9aba174d11619afbbb88a81254cb58135ce
GabrielTSR <gabrielalvesrocha22@gmail.com>
2023-01-27 20:58:34 -0300

    fix: web renders when summary is empty

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -18,7 +18,7 @@ type Summary = Array<{
 }>
 
 export function SummaryTable() {
-    const [summary, setSummary] = useState<Summary>([])
+    const [summary, setSummary] = useState<Summary>()
 
     useEffect(() => {
         api.get('summary').then((response) => {
@@ -39,7 +39,7 @@ export function SummaryTable() {
                 ))}
             </div>
             <div className="grid grid-rows-7 grid-flow-col gap-3">
-                {summary.length > 0 &&
+                {summary &&
                     summaryDates.map((date) => {
                         const dayInSummary = summary.find((day) => {
                             return dayjs(date).isSame(day.date, 'day')
